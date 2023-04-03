a432f494192dcc0074abd25848c3ed12f1b1b8c7
Jason Wang <jasonwang12@icloud.com>
2023-04-02 22:30:55 -0400

    trying

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { React, useState } from 'react';
 import Logo from '../logo.png';
+import "../styles/HomePage.css";
 
 export const HomePage = () => {
     const [clicked, setClicked] = useState(false);
@@ -10,8 +11,13 @@ export const HomePage = () => {
     return (
         <div className={`home-page ${clicked ? 'button-clicked' : ''}`}>
             <button className="logobutton" onClick={handleClick}>
-                <img src={Logo} alt="logo"></img>
+                <img className = "image" src={Logo} alt="logo"></img>
             </button>
+            {clicked && (
+            <div className="new-page">
+                <h1>Welcome to the new page!</h1>
+            </div>
+            )}
         </div>
     )
 }
