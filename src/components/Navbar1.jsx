8657722f5811dfb237c41e22c31627604471a433
Jason Wang <jasonwang12@icloud.com>
2023-04-09 17:18:48 -0400

    looks better

--- src/components/Navbar1.jsx
+++ src/components/Navbar1.jsx
@@ -6,13 +6,21 @@ import Logo from '../logo.png';
 export const Navbar1 = () => {
   return (
     <div className="navbar">
+      <Link to="/"><img className = "image" src={Logo} alt="Logo"></img></Link>
         <ul className = "Pages">
           <li>
-            <Link to="/"><img className = "image" src={Logo} alt="Logo"></img></Link>
             <Link to="/aboutme" className="navbar-link">About Me</Link>
+          </li>
+          <li>
             <Link to="/edu" className="navbar-link">Education</Link>
+          </li>
+          <li>
             <Link to="/exp" className="navbar-link">Experiences</Link>
+          </li>
+          <li>
             <Link to="/pjs" className="navbar-link">Projects</Link>
+          </li>
+          <li>
             <Link to="/tskills" className="navbar-link">Skills</Link>
           </li>
         </ul>
