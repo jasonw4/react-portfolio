b0d14da5d4e4f19fa7c1d9783ef46f2aaed8d526
Jason Wang <jasonwang12@icloud.com>
2023-04-09 19:50:46 -0400

    lol

--- src/components/Navbar1.jsx
+++ src/components/Navbar1.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
 import "../styles/Navbar1.css";
 import Logo from '../logo.png';
+import Rubiks from "../rubiks.png";
 
 export const Navbar1 = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+    const button = document.querySelector('.sidebar');
+    button.classList.toggle('spin');
+  };
   return (
-    <div className="navbar">
+    <div>
+      <div className="navbar">
       <Link to="/"><img className = "image" src={Logo} alt="Logo"></img></Link>
         <ul className = "Pages">
+        <button className="sidebar">
+          <img className="image" src={Rubiks} alt="sidebar" onClick={handleMenuToggle}></img>
+        </button>
+        </ul>
+    </div>
+    {isMenuOpen && (
+    <div className="navbar2">
+        <ul className = "Pages2">
           <li>
             <Link to="/aboutme" className="navbar-link">About Me</Link>
           </li>
@@ -24,6 +41,7 @@ export const Navbar1 = () => {
             <Link to="/tskills" className="navbar-link">Skills</Link>
           </li>
         </ul>
+    </div>)}
     </div>
   );
 }
