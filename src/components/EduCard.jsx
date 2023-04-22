f72b7be360e37eb58e812e86c00cfb1f6ab21802
Jason Wang <jasonwang12@icloud.com>
2023-04-21 22:08:21 -0400

    editing styles

--- src/components/EduCard.jsx
+++ src/components/EduCard.jsx
@@ -16,10 +16,14 @@ export const EduCard = () => {
                 <img className="logo" src={BU} alt="bu" />
             </div>
             <div className="info">
-                <h1><center>I currently attend Boston University, and will be graduating with the following:</center></h1>
-                <h3><center>Master of Science in Computer Science ~ 2025</center></h3>
-                <h3><center>Bachelor of Arts in Computer Science ~ 2025</center></h3>
-                <center><button className="button" onClick={toggleMoreInfo}>Selected Coursework</button></center>
+                {!showMoreInfo && (
+                    <>
+                        <h1><center>I currently attend Boston University, and will be graduating with the following:</center></h1>
+                        <h3><center>Master of Science in Computer Science ~ 2025</center></h3>
+                        <h3><center>Bachelor of Arts in Computer Science ~ 2025</center></h3>
+                        <center><button className="button" onClick={toggleMoreInfo}>Selected Coursework</button></center>
+                    </>
+                )}
                 {showMoreInfo && (
                     <div className="more-info">
                         <h1>Graduate</h1>
@@ -27,12 +31,13 @@ export const EduCard = () => {
                             <li>CS 542 - Intro to Machine Learning</li>
                         </ul>
                         <h1>Undergraduate</h1>
-                        <ul>
+                        <ul><center>
                             <li>CS 411 - Software Engineering</li>
                             <li>CS 112 - Intro to CS 2</li>
                             <li>CS 330 - Intro to Analysis of Algorithms</li>
                             <li>CS 237 - Probability in CS</li>
-                        </ul>
+                        </center></ul>
+                        <center><button className="button" onClick={toggleMoreInfo}>Academic Degrees</button></center>
                     </div>
                 )}
             </div>
