2da169d520d8c82898756d91154042ebffef317d
Jason Wang <jasonwang12@icloud.com>
2023-04-22 13:49:46 -0400

    skills page

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -5,7 +5,7 @@ export const SkillCard = (props) => {
     const name = props.name
     const img = props.img
     return (
-        <div className = "card">
+        <div className = "cardskill">
             <h1>{name}</h1>
             <img className="logo" src={img} alt="dne"></img>
         </div>
--- src/pages/TechnicalSkills.jsx
+++ src/pages/TechnicalSkills.jsx
@@ -1,6 +1,18 @@
 import React, { useState } from 'react';
 import "../styles/pages/TechnicalSkills.css";
 import { SkillCard } from '../components/SkillCard';
+import PY from "../images/python.gif";
+import JV from "../images/java.gif";
+import HCJ from "../images/hcj.gif";
+import SW from "../images/swift.png";
+import RT from "../images/react.gif";
+import ND from "../images/node.gif";
+import NP from "../images/numpy.png";
+import SUI from "../images/swiftui.png";
+import GT from "../images/git.png";
+import VS from "../images/vscode.png";
+import JN from "../images/jupyter.png";
+import FB from "../images/firebase.gif";
 
 export const TechnicalSkills = () => {
     const pages = [
@@ -11,15 +23,15 @@ export const TechnicalSkills = () => {
         },
         {
             title: <h1>Programming Languages</h1>,
-            skills: [<SkillCard key={1} name="Python" />, <SkillCard key={2} name="Java"/>, <SkillCard key={3} name="HTML/CSS/JS" />, <SkillCard key={4} name="Swift"/>]
+            skills: [<SkillCard key={1} name="Python" img={PY} />, <SkillCard key={2} name="Java" img={JV}/>, <SkillCard key={3} name="HTML/CSS/JS" img={HCJ}/>, <SkillCard key={4} name="Swift" img={SW}/>]
         },
         {
             title: <h1>Frameworks/Libraries</h1>,
-            skills: [<SkillCard key={1} name="React.js"/>, <SkillCard key={2} name="Node.js"/>, <SkillCard key={3} name="NumPy"/>, <SkillCard key={4} name="SwiftUI"/>]
+            skills: [<SkillCard key={1} name="React.js" img={RT}/>, <SkillCard key={2} name="Node.js" img={ND}/>, <SkillCard key={3} name="NumPy" img={NP}/>, <SkillCard key={4} name="SwiftUI" img={SUI}/>]
         },
         {
             title: <h1>Dev Tools</h1>,
-            skills: [<SkillCard key={1} name="Git/Github"/>, <SkillCard key={2} name="Visual Studio Code"/>, <SkillCard key={3} name="Jupyter Notebooks"/>, <SkillCard key={4} name="Firebase"/>]
+            skills: [<SkillCard key={1} name="Git/Github" img={GT}/>, <SkillCard key={2} name="Visual Studio Code" img={VS}/>, <SkillCard key={3} name="Jupyter Notebooks" img={JN}/>, <SkillCard key={4} name="Firebase" img={FB}/>]
         }
     ];
 
@@ -33,15 +45,15 @@ export const TechnicalSkills = () => {
                     {pages[page].title}
                     {pages[page].subTitle}
                 </center></div>
-                <div><center>
+                <div className="skillcard">
                     {pages[page].skills}
-                </center></div>
+                </div>
             </div>
             <div className="buttons">
-                <button className="button" onClick={() => setPage(0)}>Intro</button>
-                <button className="button" onClick={() => setPage(1)}>Programming Languages</button>
-                <button className="button" onClick={() => setPage(2)}>Frameworks/Libraries</button>
-                <button className="button" onClick={() => setPage(3)}>Dev Tools</button>
+                <button className="x" onClick={() => setPage(0)}>Intro</button>
+                <button className="x" onClick={() => setPage(1)}>Programming Languages</button>
+                <button className="x" onClick={() => setPage(2)}>Frameworks/Libraries</button>
+                <button className="x" onClick={() => setPage(3)}>Dev Tools</button>
             </div>
             </div>
         </div>
