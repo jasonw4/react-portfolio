c6497b74654fbfed2e2ec59a6d1cea87874245b6
Jason Wang <jasonwang12@icloud.com>
2023-04-22 14:30:29 -0400

    Version 0.0.0

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "../styles/pages/ContactMe.css";
+import IG from "../images/insta.gif";
+import GH from "../images/github.png";
+import LK from "../images/linkedin.png";
+import EM from "../images/gmail.png";
 
 export const ContactMe = () => {
   return (
     <div className="contactme">
-      <h1>Hi</h1>
+      <div className="wrapper">
+        <h1 className="content">Let's Connect!</h1>
+        <div className="contactlinks">
+          <Link to="https://www.instagram.com/jason.wang00/" target="_blank" rel="noopener noreferrer">
+            <img className="contactlink" src={IG} alt="Instagram" />
+          </Link>
+          <Link to="https://github.com/jasonw4" target="_blank" rel="noopener noreferrer">
+            <img className="contactlink" src={GH} alt="Github" />
+          </Link>
+          <Link to="https://www.linkedin.com/in/jason-wang-b9335620b/" target="_blank" rel="noopener noreferrer">
+            <img className="contactlink" src={LK} alt="LinkedIn" />
+          </Link>
+          <a href="mailto: jasonwang7374@gmail.com">
+            <img className="contactlink" src={EM} alt="Email" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
